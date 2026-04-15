ca35af193f33e1997bb5b6917f786c2f0410dc0b
FSR1184 <adrferesp@alum.us.es>
2026-04-15 09:43:59 +0200

    mensaje de hola

--- Patrones/src/factorymethod/colecciones/Colecciones.java
+++ Patrones/src/factorymethod/colecciones/Colecciones.java
@@ -26,7 +26,8 @@ import factorymethod.utilidades.MiFabrica;
  * @version 2.0 Marzo 2020
  * @author  ADRIAN FERNANDEZ ESPINOSA
  * fecha 	14/04/2026
- * uvus 	FSR1184
+ * uvus 	FSR1184 
+ * holaaaa
  */
 public class Colecciones {
     public static void main (String args[]) {	
